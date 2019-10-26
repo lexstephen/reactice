703eaf8023044146d821e3c787ba1ed9c5bc2abe
Alexis Dicks-Stephen <astephen@ola.org>
2019-10-26 00:04:45 -0400

    get a random word and display it.  hello, word!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      location: "Toronto",
+      word: "",
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMyData();
+  }
+
+  async fetchMyData() {
+    try {
+      const apiData = await axios.get("https://cors-anywhere.herokuapp.com/https://api.datamuse.com/words?topics=food", {
+        params: {
+          apikey: "65ffg8hg7fgdh"
+        }
+      });
+      const randomNumber = Math.floor(Math.random() * apiData.data.length);
+      console.log("returned "  + randomNumber, apiData.data[randomNumber]);
+      // Do whatever you need with your apiData! You'll often set it on your component state like so:
+      this.setState({
+        word: apiData.data[randomNumber].word
+      });
+    } catch(error) {
+      // add error handling here
+    }
+  }
+
+  render() {
+return (
+  <div id="la">bobob<pre>
+  {this.state.word}
+  </pre>
+  </div>)
+  }
 }
-
 export default App;
